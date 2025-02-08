03948ffdb18bad6290f0da8d9c05cc169c957c09
hidayah24 <taufik.hidayah240920@gmail.com>
2025-02-08 11:22:30 +0700

    Fix the bug

--- shortlink.js
+++ shortlink.js
@@ -1,24 +1,18 @@
 window.onload = async function () {
-    document.body.innerHTML += "<p>Script berjalan...</p>";
 
     let shortCode = window.location.pathname.substring(1);
-    document.body.innerHTML += `<p>Shortlink diminta: ${shortCode}</p>`;
 
     if (!shortCode) {
-        document.body.innerHTML += "<p>Tidak ada shortlink yang diminta. Keluar.</p>";
         return;
     }
 
     try {
-        document.body.innerHTML += "<p>Mengambil data shortlink...</p>";
 
         const response = await fetch('./shortlinks.json');
         const shortlinks = await response.json();
 
-        document.body.innerHTML += `<p>Data JSON: ${JSON.stringify(shortlinks)}</p>`;
 
         if (shortlinks[shortCode]) {
-            document.body.innerHTML += `<p>Redirecting ke: ${shortlinks[shortCode]}</p>`;
 
             // Tambahkan delay agar redirect tidak diblokir Chrome Mobile
             setTimeout(() => {
